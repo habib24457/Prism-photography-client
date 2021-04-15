d1448a938dc0e4abebcd23e853190b90ed5a023e
Habibur Rahman <rahmanhabibur24457@gmail.com>
2021-04-15 04:45:48 +0200

    added the basic files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './Components/HomePage/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
