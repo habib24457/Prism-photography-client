080a322e867c2ac83c2ab7a6a652956cee359ec8
habib24457 <habiburehman390@gmail.com>
2022-12-28 03:33:42 +0100

    Fixed login logout bug

--- src/Components/HomePage/Home/NavigationBar/NavigationBar.js
+++ src/Components/HomePage/Home/NavigationBar/NavigationBar.js
@@ -1,57 +1,117 @@
-import React from 'react';
-import {Row,Col,Navbar,Nav,Container,Button} from 'react-bootstrap';
-import {Link,useHistory} from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCameraRetro,faUsersCog,faShoppingCart,faPhone} from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from "react";
+import { Row, Col, Navbar, Nav, Container } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCameraRetro,
+  faUsersCog,
+  faShoppingCart,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavigationBar = () => {
-    const email = sessionStorage.getItem('email');
-    const history = useHistory();
-    const handlesignOut=()=>{
-        sessionStorage.removeItem('email');
-        history.replace('/home');
-    }
+  const [isLoggedin, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const history = useHistory();
 
-    return (
-        <Container>
-              <Row>
-                <Col>
-                    <Navbar collapseOnSelect expand="lg" style={{backgroundColor:' #378369'}} fixed="top">
-                        <Navbar.Brand href="/home">
-                        <FontAwesomeIcon  style={{color:'#ffffff'}} icon={faCameraRetro}/>
-                            <span  style={{color:'#ffffff',padding:'10px',textAlign:'center'}}>
-                            Prism Photography</span>
-                            </Navbar.Brand>
+  useEffect(() => {
+    const mail = sessionStorage.getItem("email");
+    setEmail(mail);
+    //console.log(email);
+  }, [isLoggedin]);
 
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                            <Nav>
-                                <Nav.Link href="/admin"> 
-                                <FontAwesomeIcon  style={{color:'#ffffff'}} icon={faUsersCog}/>
-                                <span  style={{color:'#ffffff',padding:'10px'}}>Admin</span> 
-                                </Nav.Link>
-                                <Nav.Link href="/dashboard">
-                                    <FontAwesomeIcon style={{color:'#ffffff'}}  icon={faShoppingCart}/>
-                                    <span  style={{color:'#ffffff',padding:'10px'}}>Basket</span>
-                                </Nav.Link>
-                                <Nav.Link href="/contact">
-                                <FontAwesomeIcon style={{color:'#ffffff'}}  icon={faPhone}/>
-                                    <span  style={{color:'#ffffff',padding:'10px'}}>Contact</span>
-                                    </Nav.Link>
-                                    {
-                                        email?<Button onClick={handlesignOut} variant="warning">Logout</Button>:
-                                         <Link to="/login">
-                                        <Button variant="warning">Login</Button>
-                                    </Link>
-                                    }
-                               
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </Col>
-            </Row>
-        </Container>
-    );
+  const handlesignOut = () => {
+    sessionStorage.removeItem("email");
+    setIsLoggedIn(false);
+    history.replace("/contact");
+  };
+
+  //console.log(isLoggedin, email);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Navbar
+            collapseOnSelect
+            expand="lg"
+            style={{ backgroundColor: " #378369" }}
+            fixed="top"
+          >
+            <Navbar.Brand href="/home">
+              <FontAwesomeIcon
+                style={{ color: "#ffffff" }}
+                icon={faCameraRetro}
+              />
+              <span
+                style={{
+                  color: "#ffffff",
+                  padding: "10px",
+                  textAlign: "center",
+                }}
+              >
+                Prism Photography
+              </span>
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse
+              id="responsive-navbar-nav"
+              className="justify-content-end"
+            >
+              <Nav>
+                <Nav.Link href="/checkAdmin">
+                  <FontAwesomeIcon
+                    style={{ color: "#ffffff" }}
+                    icon={faUsersCog}
+                  />
+                  <span style={{ color: "#ffffff", padding: "10px" }}>
+                    Admin
+                  </span>
+                </Nav.Link>
+                <Nav.Link href="/dashboard">
+                  <FontAwesomeIcon
+                    style={{ color: "#ffffff" }}
+                    icon={faShoppingCart}
+                  />
+                  <span style={{ color: "#ffffff", padding: "10px" }}>
+                    Basket
+                  </span>
+                </Nav.Link>
+                <Nav.Link href="/contact">
+                  <FontAwesomeIcon
+                    style={{ color: "#ffffff" }}
+                    icon={faPhone}
+                  />
+                  <span style={{ color: "#ffffff", padding: "10px" }}>
+                    Contact
+                  </span>
+                </Nav.Link>
+
+                {email ? (
+                  <button
+                    className="brand-button"
+                    onClick={() => handlesignOut()}
+                  >
+                    Logout
+                  </button>
+                ) : (
+                  <Link to="/login">
+                    <button
+                      onClick={() => setIsLoggedIn(true)}
+                      className="brand-button"
+                    >
+                      Login
+                    </button>
+                  </Link>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default NavigationBar;
